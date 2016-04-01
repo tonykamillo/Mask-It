07f158c573f77549b743231f049007281cbdd3f2
Tony Kamillo <tonysacanix@gmail.com>
2016-04-01 10:28:53 -0300

    Update jquery.mask-it.js

--- jquery.mask-it.js
+++ jquery.mask-it.js
@@ -1,27 +1,17 @@
 (function($){
-	$.fn.mask = function(pattern)
-	{
-		if( pattern && pattern.length > 0 && $(this).val().length > 0 )
-		{
-			var out = $(this).val().match(/[a-zA-Z0-9]/g);
-			var patt = pattern.split('');			
-			var marker = 0;
-			$.each(
-				pattern.match(/[^#]/g),
-				function(index, value)
-				{				
-					for(i=marker; i<patt.length; i++)
-					{					
-						if( value == patt[i] )
-						{					
-							out.splice(i, 0, value);
-							marker = i + 1;					
-							break;
-						}
-					}					
-				}
-			);
-			$(this).val(out.join(''));
-		}
-	}	
+    $.fn.mask = function(pattern)
+    {
+        if( pattern && pattern.length > 0 && $(this).val().length > 0 )
+        {
+            var patternPieces = pattern.split(''),
+                valuePieces = $(this).val().split('');
+
+            valuePieces.forEach(function(item) {
+                var i = patternPieces.indexOf('#');
+                patternPieces[i] = item;
+            });
+
+            $(this).val(patternPieces.join(''));
+        }
+    }
 })(jQuery);
